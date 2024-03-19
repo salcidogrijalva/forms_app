d46a2bec11fb253a79f8fd49930d2ed1159aeaf8
Luis Salcido <salcidogrijalva@outlook.com>
2024-03-19 13:27:59 -0700

    Custom Form Field created

---
+++ lib/presentation/widgets/inputs/custom_text_form_field.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  final String? label;
+  final String? hint;
+  final String? errorMessage;
+  final bool obscureText;
+  final Function(String)? onChanged;
+  final String? Function(String?)? validator;
+
+  const CustomTextFormField(
+      {super.key,
+      this.label,
+      this.hint,
+      this.errorMessage,
+      this.onChanged,
+      this.validator, 
+      this.obscureText = false});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    final border = OutlineInputBorder(
+      borderRadius: BorderRadius.circular(20),
+    );
+
+    return TextFormField(
+      onChanged: onChanged,
+      validator: validator,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+          enabledBorder: border,
+          focusedBorder:
+              border.copyWith(borderSide: BorderSide(color: colors.primary)),
+          errorBorder: border.copyWith(
+              borderSide: BorderSide(color: Colors.red.shade800)),
+          focusedErrorBorder: border.copyWith(
+              borderSide: BorderSide(color: Colors.red.shade800)),
+          isDense: true,
+          label: label != null ? Text(label!) : null,
+          hintText: hint,
+          errorText: errorMessage,
+          // prefixIcon: Icon(Icons.person),
+          // prefixIconColor: colors.primary
+          ),
+    );
+  }
+}
